2049d0b70df604e9eb05bfd2439caad36f2c31f2
Mayur <mayurvgowda19@gmail.com>
2024-03-15 07:34:27 -0800

    Welcome Message

--- src/Events/Member/guildMemberAdd.ts
+++ src/Events/Member/guildMemberAdd.ts
@@ -1,5 +1,5 @@
 import { Event } from "../../Interfaces/Event";
-import { ActivityType, GuildMember } from "discord.js";
+import { ActivityType, ChannelType, EmbedBuilder, GuildMember } from "discord.js";
 import { LevelDB } from "../../Models/level";
 
 const event: Event = {
@@ -11,6 +11,31 @@ const event: Event = {
             Level: 1,
             XP: 0
         });
+        const welcomeEmbed = new EmbedBuilder()
+        .setTitle(`${member.user.username}, welcome!`)
+        .setDescription(
+            `Head to <#1205964327153045515> to claim roles\nRemember no swearing!`
+        )
+        .setThumbnail(member.user.displayAvatarURL())
+        .setTimestamp()
+        .setColor("Green")
+        const welComeChannel = member.guild.channels.cache.get("1202685736109875230");
+        if(!welComeChannel || welComeChannel.type !== ChannelType.GuildText) return;
+        await welComeChannel.send({
+            content: `${member}`,
+            embeds: [welcomeEmbed]
+        });
+        // Optional
+        const memberCountChannel = member.guild.channels.cache.get("1202976070001885224");
+        if(!memberCountChannel) return;
+        const count = member.guild.members.cache.filter((m) => !m.user.bot).size;
+        await memberCountChannel.setName(`Members: ${count}`);
+        const joinRole = await member.guild.roles.fetch("1202684963846094888");
+        if(joinRole) {
+            await member.roles.add(joinRole.id);
+        } else {
+            client.sendError(`Added ${member.user.username}, but didn't find role!`, client);
+        }
     }
 }
 
